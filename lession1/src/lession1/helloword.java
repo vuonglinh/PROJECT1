444b6f603c99bc9aa903afea276a43aaad1a586d
hoang duc vuong <hdvuong2001@gmail.com>
2014-01-09 17:59:39 +0100

    MORE CHANGE TO SEE

--- lession1/src/lession1/helloword.java
+++ lession1/src/lession1/helloword.java
@@ -8,6 +8,7 @@ public class helloword {
 		System.err.println("I learn many things from you !");
 		System.err.println("Thank you very much for your time this afternoon");
 		
+		System.err.println("LEARNING BY DOING AND TEACHING YOURSELF!!!");
 		
 		
 		
