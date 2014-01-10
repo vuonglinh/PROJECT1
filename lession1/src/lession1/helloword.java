ba38b51135fbba0747c771ad1a5fab8d1764dfe3
Waseem S. <soomrowaseem@gmail.com>
2014-01-10 11:45:38 +0100

    Update helloword.java
    -

--- lession1/src/lession1/helloword.java
+++ lession1/src/lession1/helloword.java
@@ -4,7 +4,7 @@ public class helloword {
 	public static void main (String[] args){
 		System.out.println("HELLO WASEEM !");
 		System.out.println("HOW ARE YOU TODAY ?");
-		
+		// helo
 		System.err.println("Difficult to focus, so many distractions !");
 		
 		
