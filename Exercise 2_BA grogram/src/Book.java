c693bc3767cd9d2a7580996d23a491b603513847
hoang duc vuong <hdvuong2001@gmail.com>
2014-01-29 16:49:00 +0100

    DONE WITH ASSIGHMENT 2

--- Exercise 2_BA grogram/src/Book.java
+++ Exercise 2_BA grogram/src/Book.java
@@ -1,5 +1,6 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
-
 
 public class Book {
 	String Isbn;
@@ -13,115 +14,162 @@ public class Book {
 	Date ReturnDate;
 	int LibraryCardNumber;
 
-	
-	public Book(String Isbn, int CopyNumber, String Title, String Author, 
-			String Publisher, int Year, int Statistics, 
-			Date BorrowDate, Date ReturnDate, int LibraryCardNumber ){
-			
-			this.Isbn = Isbn;
-			this.CopyNumber = CopyNumber;
-			this.Title = Title;
-			this.Author = Author;
-			this.Publisher = Publisher;
-			this.Year = Year;
-			this.Statistics = Statistics;
-			this.BorrowDate = BorrowDate;
-			this.ReturnDate = ReturnDate;
-			this.LibraryCardNumber = LibraryCardNumber;
+	validateInput checkInput = new validateInput();
+
+	public Book(String Isbn, int CopyNumber, String Title, String Author,
+			String Publisher, int Year, int Statistics, Date BorrowDate,
+			Date ReturnDate, int LibraryCardNumber) {
+
+		this.Isbn = Isbn;
+		this.CopyNumber = CopyNumber;
+		this.Title = Title;
+		this.Author = Author;
+		this.Publisher = Publisher;
+		this.Year = Year;
+		this.Statistics = Statistics;
+		this.BorrowDate = BorrowDate;
+		this.ReturnDate = ReturnDate;
+		this.LibraryCardNumber = LibraryCardNumber;
 	}
 
-	public Book(String Isbn, int CopyNumber, String Title, String Author, 
-			String Publisher, int Year, int Statistics, 
-			Date BorrowDate){
-			
-			this.Isbn = Isbn;
-			this.CopyNumber = CopyNumber;
-			this.Title = Title;
-			this.Author = Author;
-			this.Publisher = Publisher;
-			this.Year = Year;
-			this.Statistics = Statistics;
-			this.BorrowDate = BorrowDate;
-				}
+	public Book(String Isbn, int CopyNumber, String Title, String Author,
+			String Publisher, int Year, int Statistics, Date BorrowDate) {
 
-	
+		this.Isbn = Isbn;
+		this.CopyNumber = CopyNumber;
+		this.Title = Title;
+		this.Author = Author;
+		this.Publisher = Publisher;
+		this.Year = Year;
+		this.Statistics = Statistics;
+		this.BorrowDate = BorrowDate;
+	}
+
 	/*
 	 * Getters and Setters
 	 */
-	
-	public String getIsbn(){
+
+	public String getIsbn() {
 		return Isbn;
 	}
-	public void setIsbn(String Isbn){
-		//check Isbn number here
-		
-		this.Isbn = Isbn;
-	
+
+	public void setIsbn(String nIsbn) {
+		if (validateInput.isISBN13Valid(nIsbn)) {
+			Isbn = nIsbn;
+		}
 	}
-	
-	public int getCopyNumber(){
+
+	public int getCopyNumber() {
 		return CopyNumber;
 	}
-	
-	public void setCopyNumber(int CopyNumber){
-		this.CopyNumber = CopyNumber;
+
+	public void setCopyNumber(String nCopyNumber) {
+
+		if (validateInput.isValidCopyNumber(nCopyNumber)) {
+			CopyNumber = Integer.parseInt(nCopyNumber);
+		}
 	}
-	
-	public String getTitle(){
+
+	public String getTitle() {
 		return Title;
 	}
-	public void setTitle(String Title){
-		this.Title = Title;
+
+	public void setTitle(String nTitle) {
+		if (validateInput.isValidTitle(nTitle)) {
+			Title = nTitle;
+		}
 	}
-	
-	public String getAuthor(){
+
+	public String getAuthor() {
 		return Author;
 	}
-	public void setAuthor(String Author){
-		this.Author = Author;
+
+	public void setAuthor(String nAuthor) {
+		if (validateInput.isValidAuthor(nAuthor)) {
+			Author = nAuthor;
+		}
+
 	}
-	
-	public String getPublisher(){
+
+	public String getPublisher() {
 		return Publisher;
 	}
-	public void setPublisher(String Publisher){
-		this.Publisher = Publisher;
+
+	public void setPublisher(String nPublisher) {
+		if (validateInput.isValidPublisher(nPublisher)) {
+			Publisher = nPublisher;
+		}
+
 	}
-	
-	public int getYear(){
+
+	public int getYear() {
 		return Year;
 	}
-	public void setYear(int Year){
-		this.Year = Year;
+
+	public void setYear(String nYear) {
+		if (validateInput.isValidYear(nYear)) {
+			Year = Integer.parseInt(nYear);
+		}
 	}
-	
-	public int getStatistics(){
+
+	public int getStatistics() {
 		return Statistics;
 	}
-	public void setStatistics(int Statistics){
-		this.Statistics = Statistics;
+
+	public void setStatistics(String nStatistics) {
+		if (validateInput.isValidStatistics(nStatistics)) {
+			Statistics = Integer.parseInt(nStatistics);
+		}
+
 	}
-	
-	public Date getBorrowDate(){
+
+	public Date getBorrowDate() {
 		return BorrowDate;
 	}
-	public void setBorrowDate(Date BorrowDate){
-		this.BorrowDate = BorrowDate;
+
+	public void setBorrowDate(String nBorrowDate) {
+		try {
+
+			SimpleDateFormat fmd = new SimpleDateFormat("yyMMdd");
+			if (validateInput.isValidBorrowDate(nBorrowDate)) {
+
+				BorrowDate = fmd.parse(nBorrowDate);
+			}
+		} catch (ParseException pex) {
+			pex.printStackTrace();
+		}
 	}
-	
-	public Date getReturnDate(){
+
+	public Date getReturnDate() {
 		return ReturnDate;
 	}
-	public void setReturnDate(Date ReturnDate){
-		this.ReturnDate = ReturnDate;
+
+	public void setReturnDate(String nReturnDate) {
+		try {
+			SimpleDateFormat df = new SimpleDateFormat("yyMMdd");
+
+			if (validateInput.isValidReturnDate(nReturnDate)) {
+				ReturnDate = df.parse(nReturnDate);
+			}
+		} catch (ParseException pex) {
+			pex.printStackTrace();
+		}
+
 	}
-	
-	public int getLibraryCardNumber(){
+
+	public int getLibraryCardNumber() {
 		return LibraryCardNumber;
 	}
-	public void setLibraryCardNumber(int LibraryCardNumber){
-		this.LibraryCardNumber = LibraryCardNumber;
+
+	public void setLibraryCardNumber(String nLibraryCardNumber) {
+		if (validateInput.isValidLibraryCardNumber(nLibraryCardNumber)) {
+			LibraryCardNumber = Integer.parseInt(nLibraryCardNumber);
+		}
 	}
-	
-	
+
+	public boolean isValidForAllDataField() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
--- Exercise 2_BA grogram/src/HandlingBook.java
+++ Exercise 2_BA grogram/src/HandlingBook.java
@@ -4,9 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.Buffer;
 import java.text.DateFormat;
-import java.text.Format;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -15,7 +13,7 @@ import java.util.FormatterClosedException;
 
 public class HandlingBook {
 
-	public ArrayList<Book> readingBook(String filename) {
+	public ArrayList<Book> readingBook(String filename) throws ParseException {
 		ArrayList<Book> bookManagement = new ArrayList<>();
 		String line = null;
 
@@ -25,17 +23,20 @@ public class HandlingBook {
 
 			while ((line = reading.readLine()) != null) {
 				Book singleBook = null;
+
 				if ((singleBook = parseToBook(line)) != null) {
-					if (singleBook.getYear() >= 2005) {
-						bookManagement.add(singleBook);
-					}
+
+					bookManagement.add(singleBook);
 				} else {
 					continue;
+
 				}
+
 			}
 			reading.close();
 		} catch (FileNotFoundException fex) {
 			System.err.println("Cannot find this file !");
+			// return null;
 			System.exit(-1);
 		} catch (IOException ex) {
 			System.err.println("Could not read this file !");
@@ -45,28 +46,97 @@ public class HandlingBook {
 		return bookManagement;
 	}
 
-	public void writeToFile(ArrayList<Book> bookList, String filename) {
+	public void writeToFile(ArrayList<Book> bookList, String fileCorrect,
+			String fileError) {
 		try {
-			BufferedWriter writing = new BufferedWriter(
-					new FileWriter(filename));
+			BufferedWriter writing1 = new BufferedWriter(new FileWriter(
+					fileCorrect));
+
+			BufferedWriter writing2 = new BufferedWriter(new FileWriter(
+					fileError));
+
+			SimpleDateFormat df = new SimpleDateFormat("yyMMdd");
+
 			for (Book eachBook : bookList) {
-				String bookLine = eachBook.getIsbn() + "#"
-						+ eachBook.getCopyNumber() + "#" + eachBook.getTitle()
-						+ "#" + eachBook.getAuthor() + "#"
-						+ eachBook.getPublisher() + "#" + eachBook.getYear()
-						+ "#" + eachBook.getStatistics() + "#"
-						+ eachBook.getBorrowDate() + "#"
-						+ eachBook.getReturnDate() + "#"
-						+ eachBook.getLibraryCardNumber();
+				String bookLine;
+
+				if (eachBook.getReturnDate() != null) {
+					bookLine = eachBook.getIsbn() + "#"
+							+ eachBook.getCopyNumber() + "#"
+							+ eachBook.getTitle() + "#" + eachBook.getAuthor()
+							+ "#" + eachBook.getPublisher() + "#"
+							+ eachBook.getYear() + "#"
+							+ eachBook.getStatistics() + "#"
+							+ df.format(eachBook.getBorrowDate()) + "#"
+							+ df.format(eachBook.getReturnDate()) + "#"
+							+ eachBook.getLibraryCardNumber();
+				} else {
+
+					bookLine = eachBook.getIsbn() + "#"
+							+ eachBook.getCopyNumber() + "#"
+							+ eachBook.getTitle() + "#" + eachBook.getAuthor()
+							+ "#" + eachBook.getPublisher() + "#"
+							+ eachBook.getYear() + "#"
+							+ eachBook.getStatistics() + "#"
+							+ df.format(eachBook.getBorrowDate());
+				}
+
+				if ((eachBook.getReturnDate() == null
+						&& eachBook.getYear() >= 2007
+						&& validateInput.isISBN13Valid(eachBook.getIsbn()) == true
+						&& validateInput.isValidCopyNumber(Integer
+								.toString((eachBook.getCopyNumber())))
+						&& validateInput.isValidTitle(eachBook.getTitle()) == true
+						&& validateInput.isValidAuthor(eachBook.getAuthor()) == true
+						&& validateInput.isValidPublisher(eachBook
+								.getPublisher()) == true
+						&& validateInput.isValidYear(Integer.toString(eachBook
+								.getYear()))
+						&& validateInput.isValidStatistics(Integer
+								.toString(eachBook.getStatistics())) && validateInput
+							.isValidBorrowDate(df.format(eachBook
+									.getBorrowDate())))
+						|| (eachBook.getReturnDate() != null
+								&& eachBook.getYear() >= 2007
+								&& validateInput.isISBN13Valid(eachBook
+										.getIsbn()) == true
+								&& validateInput.isValidCopyNumber(Integer
+										.toString((eachBook.getCopyNumber())))
+								&& validateInput.isValidTitle(eachBook
+										.getTitle()) == true
+								&& validateInput.isValidAuthor(eachBook
+										.getAuthor()) == true
+								&& validateInput.isValidPublisher(eachBook
+										.getPublisher()) == true
+								&& validateInput.isValidYear(Integer
+										.toString(eachBook.getYear()))
+								&& validateInput.isValidStatistics(Integer
+										.toString(eachBook.getStatistics()))
+								&& validateInput.isValidBorrowDate(df
+										.format(eachBook.getBorrowDate()))
+								&& validateInput.isValidReturnDate(df
+										.format(eachBook.getReturnDate())) && validateInput
+									.isValidLibraryCardNumber(Integer
+											.toString(eachBook
+													.getLibraryCardNumber())))) {
+					writing1.write(bookLine);
+					writing1.newLine();
+				} else {
+					writing2.write(bookLine);
+					writing2.newLine();
+				}
+
 			}
-			writing.close();
+			writing1.close();
+			writing2.close();
 		} catch (IOException ex) {
 			System.err.println("Cannot print this file !");
+			ex.printStackTrace();
 			System.exit(-1);
 		}
 	}
 
-	private Book parseToBook(String line) {
+	private Book parseToBook(String line) throws ParseException {
 		String[] bookLine;
 		Book eachBook = null;
 
@@ -75,43 +145,55 @@ public class HandlingBook {
 		}
 
 		if ((bookLine = line.split("#")).length == 10) {
-			int CopyNumber = Integer.parseInt(bookLine[1]);
-			int Year = Integer.parseInt(bookLine[5]);
-			int Statistics = Integer.parseInt(bookLine[6]);
-			
-			// Here I want to convert from String to Date
-			try{
-			DateFormat df = new SimpleDateFormat("yyMMDD");
-			
-			Date BorrowDate = df.parse(bookLine[7]);
-			Date ReturnDate = df.parse(bookLine[8]);
-			}catch(FormatterClosedException fex){
-				System.err.println("Cannot convert from String to Date !");
-				System.exit(-1);
+
+			try {
+				int CopyNumber = Integer.parseInt(bookLine[1]);
+				int Year = Integer.parseInt(bookLine[5]);
+				int Statistics = Integer.parseInt(bookLine[6]);
+
+				SimpleDateFormat df = new SimpleDateFormat("yyMMdd");
+				Date BorrowDate = df.parse(bookLine[7]);
+				Date ReturnDate = df.parse(bookLine[8]);
+				int LibraryCardNumber = Integer.parseInt(bookLine[9]);
+
+				eachBook = new Book(bookLine[0], CopyNumber, bookLine[2],
+						bookLine[3], bookLine[4], Year, Statistics, BorrowDate,
+						ReturnDate, LibraryCardNumber);
+
+			} catch (ParseException pex) {
+				pex.printStackTrace();
+			} catch (NumberFormatException nex) {
+//				System.err.println("Cannot convert from string to number");
+//				nex.printStackTrace();
+			return null;
+				// System.exit(-1);
+
 			}
-			int LibraryCardNumber = Integer.parseInt(bookLine[9]);
-			eachBook = new Book(bookLine[0], CopyNumber, bookLine[2],
-					bookLine[3], bookLine[4], Year, Statistics, BorrowDate, ReturnDate, LibraryCardNumber);
-		}else{
-			int CopyNumber = Integer.parseInt(bookLine[1]);
-			int Year = Integer.parseInt(bookLine[5]);
-			int Statistics = Integer.parseInt(bookLine[6]);
-			
-			try{
-			DateFormat df = new SimpleDateFormat("yyMMDD");
-			
-			Date BorrowDate = df.parse(bookLine[7]);
-			
-			}catch(FormatterClosedException fex){
-				System.err.println("Cannot convert from String to Date !");
-				System.exit(-1);
-			}
-			
-			eachBook = new Book(bookLine[0], CopyNumber, bookLine[2],
-					bookLine[3], bookLine[4], Year, Statistics, BorrowDate);
+
 		}
-		
-		
+
+		if ((bookLine = line.split("#")).length == 8) {
+
+			try {
+				int CopyNumber = Integer.parseInt(bookLine[1]);
+				int Year = Integer.parseInt(bookLine[5]);
+				int Statistics = Integer.parseInt(bookLine[6]);
+
+				SimpleDateFormat df = new SimpleDateFormat("yyMMdd");
+				Date BorrowDate = df.parse(bookLine[7]);
+				eachBook = new Book(bookLine[0], CopyNumber, bookLine[2],
+						bookLine[3], bookLine[4], Year, Statistics, BorrowDate);
+			} catch (NumberFormatException nex) {
+				//System.err.println("Cannot convert from string to number");
+				//nex.printStackTrace();
+				return null;
+
+			} catch (ParseException pex) {
+				pex.printStackTrace();
+			}
+
+		}
+
 		if (eachBook == null) {
 			return null;
 		}
